334b0b1977911bad602f0b42114a67dc320bb31e
kingsleychen <kingsamchen@gmail.com>
2015-06-07 02:25:16 +0800

    1. add basic program structure. 2. add create a new vault.

---
+++ PasswordVault.cs
@@ -0,0 +1,50 @@
+﻿/*
+ @ Kingsley Chen
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasyKeeper
+{
+    class PasswordVault
+    {
+        private class AccountInfo
+        {
+            public string UserName { get; set; }
+            public string Password { get; set; }
+
+            public AccountInfo(string userName, string password)
+            {
+                UserName = userName;
+                Password = password;
+            }
+        }
+
+        private string _path;
+        private string _accessPassword;
+        private SortedList<string, AccountInfo> _accountStore;
+
+        /// <summary>
+        /// Builds a brand new vault.
+        /// </summary>
+        public PasswordVault(string storePath, string accessPassword)
+        {
+            _path = storePath;
+            _accessPassword = accessPassword;
+            _accountStore = new SortedList<string, AccountInfo>();
+        }
+
+        /// <summary>
+        /// Builds a vault from given data.
+        /// </summary>
+        /// <param name="data"></param>
+        public PasswordVault(string storePath, string accessPassword, byte[] data)
+        {}
+
+        public void Store()
+        {}
+    }
+}
---
+++ VaultLoader.cs
@@ -0,0 +1,21 @@
+﻿/*
+ @ Kingsley Chen
+*/
+
+namespace EasyKeeper
+{
+    static class VaultLoader
+    {
+        public static PasswordVault LoadFromProvided(string path, string password)
+        {
+            return null;
+        }
+
+        public static PasswordVault LoadFromNew(string path, string password)
+        {
+            var vault = new PasswordVault(path, password);
+            vault.Store();
+            return vault;
+        }
+    }
+}
