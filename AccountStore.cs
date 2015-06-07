4f4776c483ce600cfaa96c82a06cf277354e553d
kingsleychen <kingsamchen@gmail.com>
2015-06-08 00:53:24 +0800

    1. add AccountStore for account data model 2. add VaultMarshal for data persistence.

---
+++ AccountStore.cs
@@ -0,0 +1,32 @@
+﻿/*
+ @ Kingsley Chen
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace EasyKeeper {
+    [Serializable]
+    public class AccountStore {
+        [Serializable]
+        private class AccountInfo {
+            public string UserName { get; set; }
+            public string Password { get; set; }
+
+            public AccountInfo(string userName, string password)
+            {
+                UserName = userName;
+                Password = password;
+            }
+        }
+
+        private SortedList<string, AccountInfo> _accountData;
+
+        public AccountStore()
+        {
+            _accountData = new SortedList<string, AccountInfo>();
+        }
+
+        // TODO: Model Operations
+    }
+}
---
+++ VaultMarshal.cs
@@ -0,0 +1,17 @@
+﻿/*
+ @ Kingsley Chen
+*/
+
+using System.IO;
+
+namespace EasyKeeper {
+    public static class VaultMarshal {
+        public static void Marshal(string pwd, AccountStore store, Stream stream)
+        {}
+
+        public static AccountStore Unmarshal(Stream stream, string pwd)
+        {
+            return null;
+        }
+    }
+}
