6f4fc62494616072561bf90be106500beea802b1
kingsleychen <kingsamchen@gmail.com>
2015-07-19 11:53:46 +0800

    Main structure of logging window

---
+++ LoggingWindow.xaml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EasyKeeper
+{
+    /// <summary>
+    /// Interaction logic for LoggingWindow.xaml
+    /// </summary>
+    public partial class LoggingWindow : Window
+    {
+        public LoggingWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
