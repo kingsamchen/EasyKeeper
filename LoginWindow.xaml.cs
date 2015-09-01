ea8b156f3fa34fbe8f290c7c2cc236237c938eac
kingsleychen <kingsamchen@gmail.com>
2015-09-01 09:40:18 +0800

    Remove old code-behind implementation for LoginWindow. Introducing a ViewModel instead.

--- LoginWindow.xaml.cs
+++ LoginWindow.xaml.cs
@@ -18,206 +18,75 @@ using System.Windows.Controls;
 using Win32 = Microsoft.Win32;
 
 namespace EasyKeeper {
-    public partial class LoginWindow : Window, INotifyPropertyChanged {
-        private static readonly string HistoryFilePath;
-        private List<string> _vaultLocations;
-        private readonly ObservableCollection<ComboBoxItem> _locationBoxItems;
-        private int _selectedLocationIndex = -1;
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public int SelectedLocationItemIndex
+    public partial class LoginWindow : Window {
+        public LoginWindow()
         {
-            get {
-                return _selectedLocationIndex;
-            }
+            InitializeComponent();
 
-            set {
-                _selectedLocationIndex = value;
-                OnPropertyChanged();
-            }
+            var viewModel = new LoginViewModel();
+            DataContext = viewModel;
         }
+    }
 
-        static LoginWindow()
+    class LoginViewModel : BindableObject {
+        private static readonly string HistoryFilePath;
+        private List<string> _vaultLocations = new List<string>();
+        private ObservableCollection<ComboBoxItem> _locationItems;
+        private int _selectedItemIndex = -1;
+
+        static LoginViewModel()
         {
             var appDir = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
             HistoryFilePath = Path.Combine(appDir, @"EasyKeeper\vault_history");
         }
 
-        public LoginWindow()
+        public LoginViewModel()
         {
-            InitializeComponent();
-            DataContext = this;
-
             LoadVaultLocationFromHistory();
-            var locationItems = from location in _vaultLocations
-                                select new ComboBoxItem { Content = location };
-            _locationBoxItems = new ObservableCollection<ComboBoxItem>(locationItems);
-            var chooseLocationItem = new ComboBoxItem { FontStyle = FontStyles.Italic,
-                                                        Content = FindResource("ChooseLocation") };
-            _locationBoxItems.Add(chooseLocationItem);
-            VaultLocationList.ItemsSource = _locationBoxItems;
+            _locationItems = new ObservableCollection<ComboBoxItem>(GetLocationItem());
 
             // If there are locations in history, display the first of them.
             if (_vaultLocations.Count != 0) {
-                SelectedLocationItemIndex = 0;
-            }
-        }
-
-        private void VaultLocationList_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            var locationBox = ((ComboBox)sender);
-            // When `choose a location` item is selected, display an open-file dialog for
-            // choosing another vault.
-            if (SelectedLocationItemIndex == locationBox.Items.Count - 1) {
-                var dlg = new Win32.OpenFileDialog {
-                    DefaultExt = ".ekp",
-                    Filter = "EasyKeeper vault (*.ekp)|*.ekp"
-                };
-
-                bool? result = dlg.ShowDialog();
-                if (result != true) {
-                    SelectedLocationItemIndex = -1;
-                    return;
-                }
-
-                var index = 0;
-                for (; index < _locationBoxItems.Count; ++index) {
-                    if (_locationBoxItems[index].Content.Equals(dlg.FileName)) {
-                        break;
-                    }
-                }
-
-                if (index == _locationBoxItems.Count) {
-                    _locationBoxItems.Insert(0, new ComboBoxItem { Content = dlg.FileName });
-                    index = 0;
-                }
-
-                SelectedLocationItemIndex = index;
+                SelectedItemIndex = 0;
             }
         }
 
         private void LoadVaultLocationFromHistory()
         {
-            _vaultLocations = new List<string>();
             if (File.Exists(HistoryFilePath)) {
+                _vaultLocations.Clear();
                 _vaultLocations.AddRange(File.ReadLines(HistoryFilePath, Encoding.UTF8));
             }
         }
 
-        // We always overwrite vault location history.
-        private void SaveVaultLocationHistory()
+        private IEnumerable<ComboBoxItem> GetLocationItem()
         {
-            if (_vaultLocations.Count == 0) {
-                return;
-            }
-
-            Task.Run(() => {
-                var appDataDir = Path.GetDirectoryName(HistoryFilePath);
-                Debug.Assert(appDataDir != null, "appDataDir cannot be null");
-                if (!Directory.Exists(appDataDir)) {
-                    Directory.CreateDirectory(appDataDir);
-                }
-
-                File.WriteAllLines(HistoryFilePath, _vaultLocations, Encoding.UTF8);
-            });
-        }
-
-        private void NewVault_Click(object sender, RoutedEventArgs e)
-        {
-            var dlg = new Win32.SaveFileDialog {
-                DefaultExt = ".ekp",
-                Filter = "EasyKeeper vault (*.ekp)|*.ekp"
+            var items = from location in _vaultLocations
+                        select new ComboBoxItem { Content = location };
+            var chooseLocationItem = new ComboBoxItem {
+                FontStyle = FontStyles.Italic,
+                Content = Application.Current.FindResource("ChooseLocation")
             };
 
-            bool? result = dlg.ShowDialog();
-            if (result != true) {
-                return;
-            }
-
-            var newVaultPath = dlg.FileName;
-
-            var setupPasswordDlg = new InputPasswordDialog();
-            result = setupPasswordDlg.ShowDialog();
-            if (result != true) {
-                return;
-            }
-
-            var newVaultPassword = setupPasswordDlg.NewVaultPassword;
-
-            _vaultLocations.Insert(0, newVaultPath);
-            _locationBoxItems.Insert(0, new ComboBoxItem { Content = newVaultPath });
-            SelectedLocationItemIndex = 0;
-
-            SaveVaultLocationHistory();
-
-            var passwordVault = VaultLoader.FromNew(newVaultPath, newVaultPassword);
-            var vaultViewer = new VaultViewWindow(new VaultViewModel(passwordVault));
-            vaultViewer.Show();
-            Close();
+            return items.Concat(new [] { chooseLocationItem });
         }
 
-        private void OpenVault_Click(object sender, RoutedEventArgs e)
+        public ObservableCollection<ComboBoxItem> LocationItems
         {
-            if (SelectedLocationItemIndex == -1) {
-                MessageBox.Show((string)FindResource("NoVaultSelected"), "Error",
-                                MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
-
-            if (Password.Password == string.Empty) {
-                MessageBox.Show((string)FindResource("NoPasswordGiven"), "Error",
-                                MessageBoxButton.OK, MessageBoxImage.Error);
-                Password.Focus();
-                return;
-            }
-
-            var selectedPath = (string)_locationBoxItems[SelectedLocationItemIndex].Content;
-            if (!File.Exists(selectedPath)) {
-                var result = MessageBox.Show((string)FindResource("VaultNotFound"), "Error",
-                                             MessageBoxButton.YesNo, MessageBoxImage.Question);
-                if (result == MessageBoxResult.Yes) {
-                    _locationBoxItems.RemoveAt(SelectedLocationItemIndex);
-                    // This location may not in the history.
-                    if (_vaultLocations.Remove(selectedPath)) {
-                        SaveVaultLocationHistory();
-                    }
-                }
-
-                SelectedLocationItemIndex = -1;
-                Password.Password = "";
-
-                return;
-            }
-
-            MoveVaultLocationToTop(selectedPath);
-            _locationBoxItems.Move(SelectedLocationItemIndex, 0);
-            SelectedLocationItemIndex = 0;
-
-            SaveVaultLocationHistory();
-
-            try {
-                var passwordVault = VaultLoader.FromProvided(selectedPath, Password.Password);
-                var vaultViewer = new VaultViewWindow(new VaultViewModel(passwordVault));
-                vaultViewer.Show();
-                Close();
-            } catch (IncorrectPassword ex) {
-                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            } catch (DataCorruptedException ex) {
-                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            get {
+                return _locationItems;
             }
         }
 
-        private void MoveVaultLocationToTop(string selectedPath)
+        public int SelectedItemIndex
         {
-            _vaultLocations.Remove(selectedPath);
-            _vaultLocations.Insert(0, selectedPath);
-        }
+            get {
+                return _selectedItemIndex;
+            }
 
-        private void OnPropertyChanged([CallerMemberName]string propertyName = "")
-        {
-            if (PropertyChanged != null) {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            set {
+                _selectedItemIndex = value;
+                RaisePropertyChanged();
             }
         }
     }
