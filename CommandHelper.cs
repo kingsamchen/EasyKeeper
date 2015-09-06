49aa10d4aa5707dae68ca74261768dd22cd6013f
kingsleychen <kingsamchen@gmail.com>
2015-09-07 01:38:55 +0800

    Add ExecuteCommand for providing an approach for View to execute commands in ViewModel.
    Also rename to CommandHelper.

---
+++ CommandHelper.cs
@@ -0,0 +1,75 @@
+/*
+ @ 0xCCCCCCCC
+*/
+
+using System;
+using System.Windows.Input;
+
+namespace EasyKeeper {
+    // Provides a general implementation for commands.
+    // T must be of nullable, since the `param` in methods might be null.
+    class RelayCommand<T> : ICommand {
+        private readonly Action<T> _action;
+        private readonly Predicate<T> _canExecute;
+
+        public RelayCommand(Action<T> action, Predicate<T> canExecute = null)
+        {
+            _action = action;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object param)
+        {
+            return _canExecute == null || _canExecute((T)param);
+        }
+
+        public void Execute(object param)
+        {
+            _action((T)param);
+        }
+    }
+
+    // Provides an approach for View to execute commands in ViewModel.
+    // Unlike uses of RelayCommand, these commands are manually invoked.
+    class ExecuteCommand<T, TResult> : ICommand {
+        // Unfortunately, we don't need it.
+        public event EventHandler CanExecuteChanged;
+
+        private readonly Func<T, TResult> _fn;
+        private readonly Predicate<T> _canExecute;
+        private TResult _result;
+
+        public ExecuteCommand(Func<T, TResult> fn, Predicate<T> canExecute = null)
+        {
+            _fn = fn;
+            _canExecute = canExecute;
+        }
+
+        public TResult Result
+        {
+            get { return _result; }
+        }
+
+        public bool CanExecute(object param)
+        {
+            return _canExecute == null || _canExecute((T)param);
+        }
+
+        public void Execute(object param)
+        {
+            ResetResult();
+           _result = _fn((T)param);
+        }
+
+        private void ResetResult()
+        {
+            _result = default(TResult);
+        }
+    }
+}
